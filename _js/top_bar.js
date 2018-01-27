5765c3c8321c3066bdca109167d035bc452dcbc4
Vicky Leong <vleong2332@gmail.com>
2018-01-27 10:50:07 -0500

    Add top bar with floating down text
    Since there is only one set of text, float the whole container or
    row down.
    
    The top border of the header is no longer needed.

---
+++ _js/top_bar.js
@@ -0,0 +1,7 @@
+$(function($, global) {
+
+  var topBar = $('.topBar');
+  var elementsToFloatDown = topBar.find('.startLiftedAndTransparent');
+  elementsToFloatDown.removeClass('startLiftedAndTransparent');
+
+}(jQuery, window));
