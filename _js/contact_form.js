ea0f328629dcb064b45c651f29dc21a195433593
Albert Volkman <albert.volkman@gmail.com>
2020-03-05 14:45:29 -0500

    Simplify logic for open status

--- _js/contact_form.js
+++ _js/contact_form.js
@@ -1,3 +1,13 @@
+Date.prototype.dst = function() {
+  return this.getTimezoneOffset() < this.stdTimezoneOffset();
+}
+
+Date.prototype.stdTimezoneOffset = function() {
+  var jan = new Date(this.getFullYear(), 0, 1);
+  var jul = new Date(this.getFullYear(), 6, 1);
+  return Math.max(jan.getTimezoneOffset(), jul.getTimezoneOffset());
+}
+
 $(function ($, global) {
 
   // Scope to contact forms only
@@ -5,66 +15,34 @@ $(function ($, global) {
     return;
   }
 
-  // Defaults country to the US because most inquiries come from there
-  selectCountry('United States');
-
-  getMultipleScripts([
-    'https://unpkg.com/moment@2.21.0/min/moment.min.js',
-    'https://unpkg.com/moment-timezone@0.5.14/builds/moment-timezone-with-data.min.js',
-  ]).done(function () {
-    var now = new Date();
-
-    if (norstoneIsOpen(now)) {
-      showCallUsNowText();
-    }
-  });
-
-  //////////////////////////////
-
-  function selectCountry(countryName) {
-    $('.contactForm__countrySelect')
-      .find('option[value="' + countryName + '"]')
-      .prop('selected', true);
+  if (norstoneIsOpen()) {
+    showCallUsNowText();
   }
 
   function showCallUsNowText() {
     $('.contactForm__callUsNow').removeClass('hidden');
   }
 
-  function getMultipleScripts(scriptArray) {
-    var getScriptParams = $.map(scriptArray, function (script) {
-      return $.getScript(script);
-    });
+  function norstoneIsOpen() {
+    var now = new Date();
 
-    getScriptParams.push($.Deferred(function (deferred) {
-      $(deferred.resolve);
-    }));
-
-    return $.when.apply($, getScriptParams);
-  }
-
-  function norstoneIsOpen(datetime) {
     // Monday to Friday EST
     var daysOpen = [1, 2, 3, 4, 5];
     var hourOpenEST = 9;
     var hourCloseEST = 17;
-    var norstoneIsInDST = isTimezoneInDST('America/New_York');
 
     // EST to UTC based on Daylight Savings Time
-    var hourOpenUTC = hourOpenEST + (norstoneIsInDST ? 4 : 5);
-    var hourCloseUTC = hourCloseEST + (norstoneIsInDST ? 4 : 5);
+    var hourOpenUTC = hourOpenEST + (now.dst() ? 4 : 5);
+    var hourCloseUTC = hourCloseEST + (now.dst() ? 4 : 5);
+    console.log(hourOpenUTC);
+    console.log(hourCloseUTC);
     
-    var rightNow = datetime.getUTCHours();
-    var isOpenToday = daysOpen.indexOf(datetime.getUTCDay()) !== -1;
+    var rightNow = now.getUTCHours();
+    var isOpenToday = daysOpen.indexOf(now.getUTCDay()) !== -1;
     var isOpenThisTime = rightNow >= hourOpenUTC && rightNow < hourCloseUTC;
 
     return isOpenToday && isOpenThisTime;
   }
-
-  function isTimezoneInDST(timezone) {
-    return moment(Date.now()).tz(timezone).isDST();
-  }
-
 }(jQuery, window));
 
 $(function ($, global) {
